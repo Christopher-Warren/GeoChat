a73e0433d8ca090100e65530053210f312f0c940
Christopher Warren <chrisalmith@gmail.com>
2022-08-25 23:33:45 -0400

    handle exceptions

--- contexts/UserProvider.js
+++ contexts/UserProvider.js
@@ -1,8 +1,9 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth/react-native";
 import { createContext, useState } from "react";
 import axios from "axios";
-import { ActivityIndicator, View } from "react-native";
+
 import { Loader } from "../components/loaders/Loader";
+import { Button, Text, View } from "react-native";
 
 const UserContext = createContext(null);
 const UserDispatchContext = createContext(null);
@@ -10,31 +11,47 @@ const UserDispatchContext = createContext(null);
 function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const auth = getAuth();
 
   onAuthStateChanged(auth, async (currentUser) => {
-    if (currentUser && !user) {
+    if (currentUser && !user && !error) {
       try {
-        const { data } = await axios.post("/api/verifyUid", {
-          firebaseUid: currentUser.uid,
-          phoneNumber: currentUser.phoneNumber,
-        });
+        const { data } = await axios.post(
+          "/api/verifyUid",
+          {
+            firebaseUid: currentUser.uid,
+            phoneNumber: currentUser.phoneNumber,
+          },
+          { timeout: 2000 }
+        );
 
         setUser(data.user);
+        setLoading(false);
       } catch (error) {
-        console.log("User Provider Error: ", error);
+        setError("Internal server error, please try again later");
+        setLoading(false);
       }
-
-      setLoading(false);
-    } else {
-      setLoading(false);
     }
   });
 
   return (
     <UserContext.Provider value={user}>
       <UserDispatchContext.Provider value={setUser}>
-        {loading ? <Loader /> : children}
+        {loading && <Loader />}
+        {error && (
+          <View>
+            <Text>{error}</Text>
+            <Button
+              title="Try again"
+              onPress={(e) => {
+                setError(null);
+                setLoading(true);
+              }}
+            />
+          </View>
+        )}
+        {user && children}
       </UserDispatchContext.Provider>
     </UserContext.Provider>
   );
